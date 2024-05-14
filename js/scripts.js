36648a90bdf8428ef2a7509364f3a5514a0b9b40
Breanne-Sartori <breanne.sartori@gmail.com>
2024-05-14 22:17:55 +1000

    Put Pokemon list within an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,53 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        type: ['grass', 'poison'],
-        height: 7
-    },
-    {
-        name: 'Squirtle',
-        type: 'water',
-        height: 5
-    },
-    {
-        name: 'Pidgey',
-        type: ['normal', 'flying'],
-        height: 3
-    }
-]; 
+let pokemonRepository = (function(){
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            type: ['grass', 'poison'],
+            height: 7
+        },
+        {
+            name: 'Squirtle',
+            type: 'water',
+            height: 5
+        },
+        {
+            name: 'Pidgey',
+            type: ['normal', 'flying'],
+            height: 3
+        }
+    ];
 
-//print the list of Pokemon
-/*for (let i=0; i < pokemonList.length; i++){
-    if (pokemonList[i].height > 5){
-        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - wow, that's big! " + "</p>");
+    function getAll(){
+        return pokemonList;
     }
-    else {
-    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "</p>");
-    }
-};*/
 
-pokemonList.forEach (function(pokemon){
-    if (pokemon.height > 5){
-        document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - wow, that's big! " + "</p>");
-    }
-    else {
-    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") " + "</p>");
-    }
-}); 
+    function add(pokemon){
+        if (typeof pokemon === 'object'){
+        pokemonList.push(pokemon);
+        }
+        else {
+            console.log('Not valid');
+        }
+    };
+
+    return {
+        add: add,
+        getAll: getAll
+        }
+    } ) ();
+
+//Check that the correct info is being returned
+console.log(pokemonRepository.getAll());
+
+//access the results from the key 'getAll' within the 'pokemonRepository' object
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        if (pokemon.height > 5) {
+            document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - wow, that's big! " + "</p>");
+        }
+        else {
+            document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") " + "</p>");
+        }
+    });
 
 
 
